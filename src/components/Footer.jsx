8468e2de73a115909ff55b3fd3473c677f953bf8
shiningsudipto <shiningsudipto@gmail.com>
2023-08-15 13:56:08 +0600

    remaining task and status change added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
+import { statusChanged } from "../redux/filters/actions";
+
+const numberOfTodos = (no_of_todos) => {
+    switch (no_of_todos) {
+        case 0:
+            return "No task";
+        case 1:
+            return "1 task";
+
+        default:
+            return `${no_of_todos} tasks`
+    }
+}
 
 const Footer = () => {
+
+    const filters = useSelector((state) => state.filters);
+    const todos = useSelector((state) => state.todos);
+
+    const todosRemaining = todos.filter((todo) => !todo.completed).length;
+
+    const dispatch = useDispatch();
+
+    const handleStatusChange = (status) => {
+        dispatch(statusChanged(status))
+    }
+    const { status, colors } = filters;
+
     return (
         <div className="mt-4 flex justify-between text-xs text-gray-500">
-            <p>2 tasks left</p>
+            <p>{numberOfTodos(todosRemaining)} left</p>
             <ul className="flex space-x-1 items-center text-xs">
-                <li className="cursor-pointer font-bold">All</li>
+                <li className={`cursor-pointer ${status === "All" && 'font-bold'} `}
+                    onClick={() => handleStatusChange('All')}
+                >All</li>
                 <li>|</li>
-                <li className="cursor-pointer">Incomplete</li>
+                <li className={`cursor-pointer ${status === "Incomplete" && 'font-bold'} `}
+                    onClick={() => handleStatusChange('Incomplete')}
+                >Incomplete</li>
                 <li>|</li>
-                <li className="cursor-pointer">Complete</li>
+                <li className={`cursor-pointer ${status === "Complete" && 'font-bold'} `}
+                    onClick={() => handleStatusChange('Complete')}
+                >Complete</li>
                 <li></li>
                 <li></li>
                 <li
